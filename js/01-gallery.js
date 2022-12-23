f8e72ba9a5d6aaba823410aa5d5cdbcb4fe091cb
vitalii-borisiuk <vitalii.borisiuk@gmail.com>
2022-12-23 15:33:04 +0200

    Make hw-07. First change.

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,48 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+
+
+const galleryContainer = document.querySelector('.gallery');
+const galleryMarkup = createGalleryItemMarkup(galleryItems);
+
+galleryContainer.insertAdjacentHTML('beforeend', galleryMarkup);
+galleryContainer.addEventListener('click', inModalOpen);
+
+function createGalleryItemMarkup(galleryItems) {
+    return galleryItems.map(({ preview, original, description }) =>
+    {
+   return `
+    <div class="gallery__item">
+        <a class="gallery__link" href="${original}">
+          <img
+            class="gallery__image"
+            src="${preview}"
+            data-source="${original}"
+            alt="${description}"
+          />
+        </a>
+    </div>`;
+    })
+        .join('');
+}
+function onItemClickAction (event) {
+  if(!event.target.classList.contains('gallery__item')){
+    return;
+  }
+  console.log(event.target);
+};
+
+function inModalOpen() {
+  const instance = basicLightbox.create(`
+    <div class="modal">
+        <p>
+            Your first lightbox with just a few lines of code.
+            Yes, it's really that simple.
+        </p>
+    </div>
+`);
+
+  instance.show();
+  
+};
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,34 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+// console.log(galleryItems);
+const galleryContainer = document.querySelector('.gallery');
+const galleryMarkup = createGalleryItemMarkup(galleryItems);
+
+
+
+galleryContainer.insertAdjacentHTML('beforeend', galleryMarkup);
+galleryContainer = addEventListener('click', onItemClickAction);
+
+function createGalleryItemMarkup(galleryItems) {
+    return galleryItems.map(({ preview, original, description }) =>
+    {
+   return `
+    <div class="gallery__item">
+        <a class="gallery__link" href="${original}">
+          <img
+            class="gallery__image"
+            src="${preview}"
+            data-source="${original}"
+            alt="${description}"
+          />
+        </a>
+    </div>`;
+    })
+        .join('');
+};
+function onItemClickAction (event) {
+  if(!event.target.classList.contains('gallery__item')){
+    return;
+    };
+};
